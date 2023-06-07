a8e244942d45c22a544d7c9def3afd5f00326332
Alesfjr <aalexfurtadodev@gmail.com>
2023-06-07 19:21:28 -0300

    exercicios

---
+++ exercicio1.py
@@ -0,0 +1,19 @@
+nome= 'Alex'
+sobrenome= 'Furtado'
+idade=24
+nascimento=2022-24
+m_idade= idade >= 18
+altura= 1.83
+
+
+
+
+
+
+print('Nome:',nome)
+print('Sobrenome',sobrenome)
+print('Idade:',idade)
+print('Ano de nacimento:',nascimento)
+print('é maior de idade:',m_idade)
+print('Altura em metros:',altura)
+
---
+++ exercicio2.py
@@ -0,0 +1,7 @@
+nome = 'Alex'
+altura=1.83
+peso=63
+imc= peso//(altura*altura)
+
+# o f antes da string serve para colocar variaveis dentro da string
+print(f'{nome} tem {altura} de altura, pesa {peso} kg e seu imc é de {imc:.2f}') #.2f serve para reduzir oque vem depois da virgula
