047aaf2b3256bfa084c0bf80c7403e2c9ecacf7f
Alesfjr <aalexfurtadodev@gmail.com>
2023-06-19 17:24:32 -0300

    at

--- aula/aulaTRY.py
+++ aula/aulaTRY.py
@@ -1,3 +1,15 @@
-numero=int(input('Vou dobrar o numero que voce digitar:'))
+'''
+não é  desta maneira que se usa TRY E EXCEPT
+'''
+numero_str=input('Vou dobrar o numero que voce digitar:')
 
-print(f'O dobro de {numero} é {numero*2}')
+
+
+try :
+    print('  STR:',numero_str)
+    numero_int=float(numero_str)
+    print('FLOAT:',numero_int)
+    print(f'O dobro de {numero_str} é {numero_int*2}')
+
+except:
+    print('Isso não é um numero')
