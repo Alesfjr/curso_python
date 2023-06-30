35746b080fb3249d7312fb271a0d2ca509fa25bf
Alesfjr <aalexfurtadodev@gmail.com>
2023-06-30 01:48:52 -0300

    Create caculator.py

---
+++ calculadora/caculator.py
@@ -0,0 +1,19 @@
+n1=float(input ('Digite um numero:'))
+operador=input ('Digite um operador:')
+n2=float(input ('Digite um outro numero :'))
+
+if operador =='+':
+    resultado1=n1+n2
+    print(resultado1)
+elif operador =='-':
+    resultado2=n1-n2
+    print(resultado3)
+elif operador =='*':
+    resultado3=n1*n2
+    print(resultado3)
+elif operador =='/':
+    resultado4=n1/n2
+    print(resultado4)
+
+else:
+    print('Falta de informação')
