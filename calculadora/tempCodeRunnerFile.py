5b64ac245a498d23e58907463fe8e9e41698646d
Alesfjr <aalexfurtadodev@gmail.com>
2023-07-04 18:40:16 -0300

    att

---
+++ calculadora/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+if operador =='+':
+    resultado1=n1+n2
+    print(resultado1)
+elif operador =='-':
+    resultado2=n1-n2
+    print(resultado3)
+elif operador =='*':
+    resultado3=n1*n2
+    print(resultado3)
+elif operador =='/':
+    resultado4=n1/n2
+    print(resultado4)
