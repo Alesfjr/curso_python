2a30f74a7156a30cc50a0479343c71968e2fad01
Alesfjr <aalexfurtadodev@gmail.com>
2023-07-04 21:34:40 -0300

    Create quatidadelaetra.py

---
+++ quatidadelaetra.py
@@ -0,0 +1,25 @@
+frase='Teria maior confiança no desempenho de um homem que espera ter uma grande recompensa do que no daquele que já a recebeu/Não devemos de forma alguma preocupar-nos com o que diz a maioria, mas apenas com a opinião dos que têm conhecimento do justo e do injusto, e com a própria verdade.'
+
+i=0
+qtd_apareceu_mais=0
+letra_apareceu_mais_vezes=''
+while i<len(frase):
+    letra_atual=frase[i]
+    if letra_atual=='':
+        i+=1
+        continue
+
+    quatas_vezes_letra_apareceu=frase.count(letra_atual)
+
+    if qtd_apareceu_mais<quatas_vezes_letra_apareceu:
+
+        qtd_apareceu_mais=quatas_vezes_letra_apareceu
+
+        letra_apareceu_mais_vezes=letra_atual
+
+
+  
+    
+print('A letra que apareceu mais vezes'
+    f'{letra_apareceu_mais_vezes}'
+    f'que apareceu{qtd_apareceu_mais}')
