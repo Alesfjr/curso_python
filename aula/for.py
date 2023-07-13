05bc20668560aca4a1cb8091af936f549ae283fb
Alesfjr <aalexfurtadodev@gmail.com>
2023-07-13 18:39:33 -0300

    att

--- aula/for.py
+++ aula/for.py
@@ -2,4 +2,9 @@ palavra='eu/acho/que/estou/entendendo '
 espace=''
 
 for letras in palavra:
+    
+    espace += f'*{letras}'
+
     print(letras)
+
+print(espace)
---
+++ aula/range.py
@@ -0,0 +1,11 @@
+"""
+For + Range
+range -> range(start, stop, step)
+"""
+
+numeros=range(0,10,2)
+nana='nanaunaha'
+
+
+for valor in numeros:
+    print(valor)
